d2e133bbf7f5258ef149c69c4812207730769679
Harshad1863 <70088985+Harshad1863@users.noreply.github.com>
2021-04-12 11:48:52 +0530

    Done event getting

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 x = pygame.init()
 
-#windows
+#Windows
 gameWindow = pygame.display.set_mode((1200,500)) 
-#Title  
-pygame.display.set_caption("Flippy Bird")         
+#Set Title  
+pygame.display.set_caption("Flippy Bird")        
 
 # Game specific Variable
 exit_game = False
 game_over = False
 
+#Creating a game loop
+while not exit_game:
+    for event in pygame.event.get():
+        print(event)
+
+
+
+pygame.quit()
+quit()
 
