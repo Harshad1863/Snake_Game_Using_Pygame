2f81dbe06430085a05e477c62cb2992fab9d9186
Harshad1863 <70088985+Harshad1863@users.noreply.github.com>
2021-04-12 12:41:25 +0530

    Arrow Keys direction defined

--- snake.py
+++ snake.py
@@ -21,6 +21,10 @@ game_over = False
 snake_x=45
 snake_y=55
 snake_size=10
+fps=30  #frame per sec
+
+clock = pygame.time.Clock()
+
 
 
 #Game loop
@@ -28,12 +32,35 @@ while not exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
+        
+        if event.type == pygame.KEYDOWN:
+            #for Right arrow key
+            if event.key == pygame.K_RIGHT:
+                snake_x = snake_x + 10
+
+            #for Left arrow key
+            if event.key == pygame.K_LEFT:
+                snake_x = snake_x - 10
+            
+            #for UP arrow key
+            if event.key == pygame.K_UP:
+                snake_y = snake_y - 10
+
+            #for Down arrow key
+            if event.key == pygame.K_DOWN:
+                snake_y = snake_y + 10
+
+            
+
+            
+
 
 
 
     gameWindow.fill(white) 
     pygame.draw.rect(gameWindow,black, [snake_x, snake_y, snake_size , snake_size ])  
     pygame.display.update()
+    clock.tick(fps)
 
 
 pygame.quit()
