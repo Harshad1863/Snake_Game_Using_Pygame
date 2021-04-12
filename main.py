bbca1f29e807f0a264e195360b43f2b71d17d8c7
Harshad1863 <70088985+Harshad1863@users.noreply.github.com>
2021-04-12 11:37:30 +0530

    Initial setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+x = pygame.init()
+
+#windows
+gameWindow = pygame.display.set_mode((1200,500)) 
+#Title  
+pygame.display.set_caption("Flippy Bird")         
+
+# Game specific Variable
+exit_game = False
+game_over = False
+
+
